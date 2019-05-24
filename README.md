503edff35192f21fb0416bd389a00f6dcac0a9b9
Gilles Didier <gilles.didier@free.fr>
2019-05-24 10:45:50 +0200

    revision

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 R-scripts to test for correlation between traits under directional evolution
 
-They require the packages ape, ade4, lmtest, tseries, caper
+They require the packages ape, ade4, caper, lmtest, tseries
 
 All the functions take as arguments a phylogeny "phy" which has to be provided 
 as an object of type "phylo" and one or two numeric vectors X (and Y) storing 
@@ -9,26 +9,26 @@ consistent with those of the tips of "phy").
 
 Trend-detection tests
 
-SR_trend.test takes as argument a phylogeny phy and a vector X and returns the 
+SR_trend.test takes as argument a phylogeny phy and a vector X and returns 
 Pearson's correlation test between the tip values and their times in order to 
 detect a trend in the trait evolution and the results of the diagnostic tests 
 for least squares regression validity conditions (Durbin-Watson's, 
 Harrison-McCabe's and Jarque-Bera's tests).
 
-hR_trend.test takes as argument phy and X and returns the Pearson's correlation 
-test between the independent contrasts and the "h_k" in order to detect a trend 
-in the trait evolution and the results of the diagnostic tests for least squares 
-regression validity conditions (Durbin-Watson's, Harrison-McCabe's and 
+hR_trend.test takes as argument phy and X and returns the test of the nullity of the slope
+when regressing the independent contrasts on the "h_k", in order to detect a trend 
+in the trait evolution. This function also provides the results of the diagnostic tests 
+for least squares regression validity conditions (Durbin-Watson's, Harrison-McCabe's and 
 Jarque-Bera's tests).
 
 Correlation tests
 
-SR_corr.test, IC_corr.test, DC_corr.test and MR_corr.test take as argument a 
-phylogeny phy and two vectors X and Y and implements the SR, IC, DC and MR tests 
-as described in the manuscript. They returns the corresponding Pearson's 
-correlation test and the results of the diagnostic tests for least squares 
-regression validity conditions (Durbin-Watson's, Harrison-McCabe's and 
-Jarque-Bera's tests).
+SR_corr.test, IC_corr.test, PGLS_corr.test, DC_corr.test, PGLSt_corr.test and MR_corr.test 
+take as argument a phylogeny phy and two vectors X and Y and implement the SR, IC, DC and MR tests 
+as described in the manuscript. They return the corresponding test of the nullity of the slope in the
+associated regression in order to test for correlation. These functions also provide the results 
+of the diagnostic tests for least squares regression validity conditions (Durbin-Watson's, 
+Harrison-McCabe's and Jarque-Bera's tests).
 
 
 The script in the file "simulations.R" compute all the figures of the manuscript. 
--- scripts/correlation_&_trend_detection.R
+++ scripts/correlation_&_trend_detection.R
@@ -1,10 +1,12 @@
 
 
-library(ape)
+
 library(ade4)
-library(lmtest)
+library(ape)
 library(tseries)
+library(lmtest)
 library(caper)
+
 
 
 
@@ -19,7 +21,6 @@ library(caper)
 # X 	a vector of mode numeric giving the values of a first trait for the tips.
 # 	Its values are taken to be in the same order than the tip labels of phy
 
-# Y 	a vector of mode numeric giving the values of a second trait for the tips.
 # 	Its values are taken to be in the same order than the tip labels of phy
 
 
@@ -33,29 +34,28 @@ SR_trend.test <- function(phy, X)
 {
 	nT <- length(phy$tip.label)
 	AllTimes <- dist.nodes(phy)
-	TL <- AllTimes[(nT+1),1:nT]
-	names(TL) <- phy$tip.label
-	resSR <- lm(X ~ TL)
-	temp <- summary(resSR)$coef[2,4]
- 	JB <-  jarque.bera.test(summary(resSR)$res)$p.val
-	DW <- dwtest(resSR, order.by=TL)$p.val
-	HMC <- hmctest(resSR, order.by=TL, nsim=10000)$p.val
+	AT <- AllTimes[(nT+1),1:nT]
+	names(AT) <- phy$tip.label
+	resTR <- lm(X ~ AT)
+	temp <- summary(resTR)$coef[2,4]
+ 	JB <-  jarque.bera.test(summary(resTR)$res)$p.val
+	DW <- dwtest(resTR, order.by=TL)$p.val
+	HMC <- hmctest(resTR, order.by=TL, nsim=10000)$p.val
 	vec <- c(temp, JB, DW, HMC)
  	names(vec) <- c("SR", "Jarque-Bera", "Durbin-Watson", "Harrison-McCabe")
-	print("p-values for SR trend-detection test and for conditions checking")
+	print("p-values for SR trend-detection test with conditions checking")
 	return(vec)
 }
 
 
-hR_trend.test <- function(phy, X)
+hR_trend.test <- function(phy, X, Y)
 {
 	pic.X <- pic(X, phy)
-	pic.Y <- pic(Y, phy)
 	nT <- length(phy$tip.label)
 	AllTimes <- dist.nodes(phy)
-	TL <- AllTimes[(nT+1),1:nT]
-	names(TL) <- phy$tip.label
-	hk <- pic(TL, phy)
+	AT <- AllTimes[(nT+1),1:nT]
+	names(AT) <- phy$tip.label
+	hk <- pic(AT, phy)
 	MUchapX <- sum(pic.X* hk) / sum(hk^2)
 	reshR <- lm(pic.X ~ hk - 1)
 	temp <- c(MUchapX, summary(reshR)$coef[1,4])
@@ -64,7 +64,7 @@ hR_trend.test <- function(phy, X)
 	HMC <- hmctest(reshR, order.by=hk, nsim=10000)$p.val
 	vec <- c(temp, JB, DW, HMC)
  	names(vec) <- c("trend-estimate", "hR", "Jarque-Bera", "Durbin-Watson", "Harrison-McCabe")
-	print("trend estimation and p-values for hR trend-detection test and for conditions checking")
+	print("trend estimation and p-values for hR trend-detection test with conditions checking")
 	return(vec)
 }
 
@@ -83,14 +83,14 @@ hR_trend.test <- function(phy, X)
 
 SR_corr.test <- function(phy, X, Y)
 {
-	resSR <- lm(Y ~ X)
-	temp <- summary(resSR)$coef[2,4]
-	JB <-  jarque.bera.test(summary(resSR)$res)$p.val
-	DW <- dwtest(resSR, order.by=X)$p.val
-	HMC <- hmctest(resSR, order.by=X, nsim=10000)$p.val
+	resTR <- lm(Y ~ X)
+	temp <- summary(resTR)$coef[2,4]
+	JB <-  jarque.bera.test(summary(resTR)$res)$p.val
+	DW <- dwtest(resTR, order.by=X)$p.val
+	HMC <- hmctest(resTR, order.by=X, nsim=10000)$p.val
 	vec <- c(temp, JB, DW, HMC)
  	names(vec) <- c("SR", "Jarque-Bera", "Durbin-Watson", "Harrison-McCabe")
-	print("p-values for SR correlation test and for conditions checking")
+	print("p-values for SR correlation test with conditions checking")
 	return(vec)
 }
 
@@ -99,41 +99,59 @@ IC_corr.test <- function(phy, X, Y)
 {
 	pic.X <- pic(X, phy)
 	pic.Y <- pic(Y, phy)
-	resIC <- lm(pic.Y ~ pic.X - 1)
-	temp <- summary(resIC)$coef[1,4]
-	JB <-  jarque.bera.test(summary(resIC)$res)$p.val
-	DW <- dwtest(resIC, order.by=pic.X)$p.val
-	HMC <- hmctest(resIC, order.by=pic.X, nsim=10000)$p.val
+	resCR <- lm(pic.Y ~ pic.X - 1)
+	temp <- summary(resCR)$coef[1,4]
+	JB <-  jarque.bera.test(summary(resCR)$res)$p.val
+	DW <- dwtest(resCR, order.by=pic.X)$p.val
+	HMC <- hmctest(resCR, order.by=pic.X, nsim=10000)$p.val
 	vec <- c(temp, JB, DW, HMC)
  	names(vec) <- c("IC", "Jarque-Bera", "Durbin-Watson", "Harrison-McCabe")
-	print("p-values for IC correlation test and for conditions checking")
+	print("p-values for IC correlation test with conditions checking")
 	return(vec)
 }
 
 
-DC_corr.test <- function(phy, X, Y)
+PGLS_corr.test <- function(phy, X, Y)
+{
+  DF <-  data.frame(Species=phy$tip.label, X=X, Y=Y)
+  rownames(DF) <- phy$tip.label
+  nT <- length(phy$tip.label)
+  Global <- comparative.data(phy, DF, "Species")
+  resPR <- pgls(Y ~ X, data=Global)
+  temp <- summary(resPR)$coef[2,4]
+  JB <-  jarque.bera.test(summary(resPR)$res)$p.val
+  DW <- dwtest(resPR, order.by=X)$p.val
+  HMC <- hmctest(resPR, order.by=X, nsim=10000)$p.val
+  vec <- c(temp, JB, DW, HMC)
+  names(vec) <- c("PGLS", "Jarque-Bera", "Durbin-Watson", "Harrison-McCabe")
+  print("p-values for PR correlation test with conditions checking")
+  return(vec)
+}  
+
+DR_corr.test <- function(phy, X, Y)
 {
 	pic.X <- pic(X, phy)
 	pic.Y <- pic(Y, phy)
 	nT <- length(phy$tip.label)
 	AllTimes <- dist.nodes(phy)
-	TL <- AllTimes[(nT+1),1:nT]
-	names(TL) <- phy$tip.label
-	hk <- pic(TL, phy)
+	AT <- AllTimes[(nT+1),1:nT]
+	names(AT) <- phy$tip.label
+	hk <- pic(AT, phy)
 	MUchapX <- sum(pic.X* hk) / sum(hk^2)
 	MUchapY <- sum(pic.Y* hk) / sum(hk^2)
 	pic.X.mod <- pic.X - MUchapX * hk
 	pic.Y.mod <- pic.Y - MUchapY * hk
-	resDC <- lm(pic.Y.mod ~ pic.X.mod)
-	temp <- summary(resDC)$coef[2,4]
-	JB <-  jarque.bera.test(summary(resDC)$res)$p.val
-	DW <- dwtest(resDC, order.by=pic.X.mod)$p.val
-	HMC <- hmctest(resDC, order.by=pic.X.mod, nsim=10000)$p.val
+	resDCR <- lm(pic.Y.mod ~ pic.X.mod)
+	temp <- summary(resDCR)$coef[2,4]
+	JB <-  jarque.bera.test(summary(resDCR)$res)$p.val
+	DW <- dwtest(resDCR, order.by=pic.X.mod)$p.val
+	HMC <- hmctest(resDCR, order.by=pic.X.mod, nsim=10000)$p.val
 	vec <- c(temp, JB, DW, HMC)
- 	names(vec) <- c("DC", "Jarque-Bera", "Durbin-Watson", "Harrison-McCabe")
-	print("p-values for DC correlation test and for conditions checking")
+ 	names(vec) <- c("DR", "Jarque-Bera", "Durbin-Watson", "Harrison-McCabe")
+	print("p-values for DR correlation test with conditions checking")
 	return(vec)
 }
+
 
 
 MR_corr.test <- function(phy, X, Y)
@@ -142,29 +160,55 @@ MR_corr.test <- function(phy, X, Y)
 	pic.Y <- pic(Y, phy)
 	nT <- length(phy$tip.label)
 	AllTimes <- dist.nodes(phy)
-	TL <- AllTimes[(nT+1),1:nT]
-	names(TL) <- phy$tip.label
-	hk <- pic(TL, phy)
-	resMR <- lm(pic.Y ~ pic.X + hk - 1)
-	temp <- summary(resMR)$coef[1,4]
-	JB <-  jarque.bera.test(summary(resMR)$res)$p.val
+	AT <- AllTimes[(nT+1),1:nT]
+	names(AT) <- phy$tip.label
+	hk <- pic(AT, phy)
+	resMCR <- lm(pic.Y ~ pic.X + hk - 1)
+	temp <- summary(resMCR)$coef[1,4]
+	JB <-  jarque.bera.test(summary(resMCR)$res)$p.val
 	DFuh <- data.frame(UX=pic.X, hk=hk)
-	acppc <- dudi.pca(DFuh, center=T, scale=F, scannf = F, nf = ncol(DFuh))
+	acppc <- dudi.pca(DFuh, center=T, scale=F, scannf=F, nf=ncol(DFuh))
 	nbObs <- nrow(DFuh)
 	scores <- rep(NA, length=nbObs)
 	for (il in 1 :nbObs)
 	{
 		scores[il] <- sum(acppc$eig*acppc$li[il,]^2)/nbObs
 	}
-	DW <- dwtest(resMR, order.by=scores)$p.val
-	HMC <- hmctest(resMR, order.by=scores, nsim=10000)$p.val
+	DW <- dwtest(resMCR, order.by=scores)$p.val
+	HMC <- hmctest(resMCR, order.by=scores, nsim=10000)$p.val
 	vec <- c(temp, JB, DW, HMC)
  	names(vec) <- c("MR", "Jarque-Bera", "Durbin-Watson", "Harrison-McCabe")
-	print("p-values for MR correlation test and for conditions checking")
+	print("p-values for MR correlation test with conditions checking")
 	return(vec)
 }
 
-	
+
+PGLSt_corr.test <- function(phy, X, Y)
+{
+  DF <-  data.frame(Species=phy$tip.label, X=X, Y=Y)
+  rownames(DF) <- phy$tip.label
+  nT <- length(phy$tip.label)
+  Global <- comparative.data(phy, DF, "Species")
+  AllTimes <- dist.nodes(phy)
+  AT <- AllTimes[(nT+1),1:nT]
+  resMPR <- pgls(Y ~ X + AT, data=Global)
+  temp <- summary(resMPR)$coef[2,4]
+  JB <-  jarque.bera.test(summary(resMPR)$res)$p.val
+  DFxt <- data.frame(X=X, Time=AT)
+  acppc <- dudi.pca(DFxt, center=T, scale=F, scannf=F, nf=ncol(DFxt))
+  nbObs <- nrow(DFxt)
+  scores <- rep(NA, length=nbObs)
+  for (il in 1 :nbObs)
+  {
+    scores[il] <- sum(acppc$eig*acppc$li[il,]^2)/nbObs
+  }
+  DW <- dwtest(resMPR, order.by=scores)$p.val
+  HMC <- hmctest(resMPR, order.by=scores, nsim=10000)$p.val
+  vec <- c(temp, JB, DW, HMC)
+  names(vec) <- c("PGLSt", "Jarque-Bera", "Durbin-Watson", "Harrison-McCabe")
+  print("p-values for PGLSt correlation test with conditions checking")
+  return(vec)
+}  	
 
 
 
